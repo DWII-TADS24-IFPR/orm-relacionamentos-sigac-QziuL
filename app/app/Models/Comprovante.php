ebbd65c3ba3b08f24b9852a6703ba4ef349ffbdc
QziuL <lf.quinholi2@gmail.com>
2025-04-20 17:16:25 -0300

    feat: add values fillables and relationships: categoria, aluno, user

--- app/app/Models/Comprovante.php
+++ app/app/Models/Comprovante.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comprovante extends Model
 {
-    //
+    protected $table = 'comprovantes';
+
+    protected $fillable = [
+        'horas',
+        'atividade',
+        'categoria_id',
+        'aluno_id',
+        'user_id'
+    ];
+
+    public function categoria(): BelongsTo{
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function aluno(): BelongsTo{
+        return $this->belongsTo(Aluno::class);
+    }
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
