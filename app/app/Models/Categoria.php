187c40d1c54f937554b0f9a932a557936f8581de
QziuL <lf.quinholi2@gmail.com>
2025-04-20 17:15:03 -0300

    feat: add model and migration for categoria with relationships

---
+++ app/app/Models/Categoria.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Categoria extends Model
+{
+    protected $table = 'categorias';
+
+    protected $fillable = ['nome', 'curso_id', 'maximo_horas'];
+
+    public function comprovantes(): HasMany{
+        return $this->hasMany(Comprovante::class);
+    }
+
+    public function curso(): BelongsTo{
+        return $this->belongsTo(Curso::class);
+    }
+}
