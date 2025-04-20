25f72aa6ecdfafc5565ffcf196a7db55ecbf2112
QziuL <lf.quinholi2@gmail.com>
2025-04-20 15:21:09 -0300

    first commit

---
+++ app/database/migrations/2025_04_16_142439_create_comprovantes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comprovantes', function (Blueprint $table) {
+            $table->id();
+            $table->float('horas');
+            $table->string('atividade');
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('aluno_id');
+            $table->unsignedBigInteger('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comprovantes');
+    }
+};
