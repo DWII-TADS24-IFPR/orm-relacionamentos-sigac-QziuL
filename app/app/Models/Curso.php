6b2885537837618acf9b49dcfb7584715340ee3a
QziuL <lf.quinholi2@gmail.com>
2025-04-20 16:57:46 -0300

    feat: relationship between curso, eixo and nivel

--- app/app/Models/Curso.php
+++ app/app/Models/Curso.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -24,5 +25,13 @@ class Curso extends Model
 
     public function alunos(): HasMany{
         return $this->HasMany(Aluno::class);
+    }
+
+    public function nivel(): BelongsTo{
+        return $this->belongsTo(Nivel::class);
+    }
+
+    public function eixo(): BelongsTo{
+        return $this->belongsTo(Eixo::class);
     }
 }
