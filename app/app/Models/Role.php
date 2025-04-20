34e6b0a5880d9cae84786382c99c35083263463c
QziuL <lf.quinholi2@gmail.com>
2025-04-20 16:27:06 -0300

    feat: add relationship role<-permission->resource

--- app/app/Models/Role.php
+++ app/app/Models/Role.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
-    //
+    protected $table = 'roles';
+
+    protected $fillable = ['nome'];
+
+    public function permissoes(): HasMany{
+        return $this->hasMany(Permission::class);
+    }
 }
